d5c3d6983e8ec692d5b17149f6040ab80cba3a52
Prithvi Deshpande <deshpandeprithvi@gmail.com>
2020-10-24 02:54:32 +0530

    Waypoint Nav complete
    Not tested

--- scripts/waypointNav.py
+++ scripts/waypointNav.py
@@ -17,15 +17,23 @@ reachable=None
 
 def waypointSplitter():
     global currPos,goalPos,waypoints
-    waypoints.append(goalPos)
+    # waypoints.append(goalPos)
     temp=currPos
+    n= abs(goalPos.position.x-currPos.position.x)
+    slope= goalPos.position.y-currPos.position.y/goalPos.position.x-currPos.position.x
+    xPoints= linspace(currPos,goalPos,n)
+    for i in xPoints:
+        temp=Pose()
+        temp.position.x= i
+        temp.position.y=(i*slope)
+        waypoints.append(temp)
     # i=0.3 if currPos.position.x>=goalPos.position.x else -0.3
     # for temp.position.x in range(currPos.position.x,goalPos.position.x): 
     #     temp.position.x=currPos.position.x+i
     # i=temp.position.y/temp.position.x if currPos.position.y>=goalPos.position.y else -temp.position.y/temp.position.x
     # for temp.position.y in range(currPos.position.y,goalPos.position.y): 
     #     temp.position.y=currPos.position.y+i
-    waypoints.append(temp)    
+    # waypoints.append(temp)    
 
 def unreachableCheck(msg):
     global reachable
@@ -45,20 +53,32 @@ def goalStack():
             goalPublisher.publish(goal)
             rospy.loginfo("Published {} {}".format(goal.position.x,goal.position.y))
             waypoints.pop()
-    #goalGen()
+    # goalGen() ??
 
-# def goalGen():
-#     currPos.position.x=curr_x
-#     currPos.position.y=curr_y
-#     goalPos.position.x= random.randrange(currPos.position.x-3,currPos.position.x+3,1)
-#     goalPos.position.y= random.randrange(currPos.position.y-3,currPos.position.y+3,1)
-#     waypoints.append(goalPos)
+def goalGen():
+    global currPos,goalPos
+    goalPos.position.x= random.randrange(currPos.position.x-3,currPos.position.x+3,1)
+    goalPos.position.y= random.randrange(currPos.position.y-3,currPos.position.y+3,1)
+    waypoints.append(goalPos)
+    waypointSplitter()
 
 
 def listener():
+    global currPos
     rospy.init_node('ria_wayPointPublisher',anonymous=True)
     rospy.Subscriber('/ria_nav/unreachableCheck',Bool,unreachableCheck)
     listener= tf.TransformListener()
+    while not rospy.is_shutdown():
+        try:
+            (lin,rot)= listener.lookupTransform('/odom','/camera_link',rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+    currPos.position.x= lin[0]
+    currPos.position.y= lin[1]
+    goalGen()
+    goatlStack()
+
+
 
 if __name__=='__main__':
     listener()
