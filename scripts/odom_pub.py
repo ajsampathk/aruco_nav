b14190951e920c2ceb3311528ce02ded2698a72b
Abhijith S <aj.sampathk@gmail.com>
2021-01-20 18:12:20 +0000

    docking code

--- scripts/odom_pub.py
+++ scripts/odom_pub.py
@@ -2,16 +2,20 @@
 import rospy
 from std_srvs.srv import SetBool, SetBoolResponse, Trigger, TriggerResponse
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Bool
 
 pub= rospy.Publisher("/dynamixel_workbench/cmd_vel",Twist,queue_size=1)
+goal_fb = rospy.Publisher("/ria/odom/goal_fb", Bool, queue_size=1)
 goal = [0,0]
 rotate, stop, reset = True, False, False
+new_goal=False
 
 def goal_stop(msg):
     global stop
     stop = msg.data
     response = SetBoolResponse()
     response.success = True
+    response.message = "{} ".format(stop)
     rospy.loginfo("Goal Stopped Set: {}".format(stop))
     return response
 
@@ -32,18 +36,20 @@ def goal_reset(msg):
     return response
 
 def goalCallback(msg):
-    global goal,rotate
+    global goal,rotate,new_goal
     goal[0] = msg.linear.x
     goal[1] = msg.angular.z
     rotate=True
+    new_goal=True
     rospy.loginfo("Goal:{}".format(goal))
 
 def poseCallback(msg):
-   global goal,pub,rotate, stop
+   global goal,pub,rotate, stop, new_goal
    zero = Twist()
    ang = Twist()
    lin = Twist()
-   if rotate:
+   #rospy.loginfo("{},{},{}".format(stop,rotate,goal))
+   if new_goal and rotate:
        th_diff=goal[1]-msg.angular.z
        if abs(th_diff)<0.015:
          pub.publish(zero)
@@ -58,20 +64,23 @@ def poseCallback(msg):
          else:
              pub.publish(ang)
        #rospy.loginfo("Theta Diff:{}".format(th_diff))
-   else:
+   elif new_goal:
         l_diff = goal[0]-msg.linear.x
-        if abs(l_diff)<0.025:
+        if new_goal and abs(l_diff)<0.025:
             pub.publish(zero)
+            goal_fb.publish(True)
+            new_goal=False
             rotate=True
         else:
            if l_diff<0:
                lin.angular.z=-0.2
+           else:
                lin.angular.z=0.2
            if stop:
                pub.publish(zero)
            else:
                pub.publish(lin)
-        #rospy.loginfo("Linear diff: {}".format(l_diff))
+        rospy.loginfo("Linear diff: {}".format(l_diff))
 
 def listener():
     rospy.init_node('ria_manualControl',anonymous=True)
